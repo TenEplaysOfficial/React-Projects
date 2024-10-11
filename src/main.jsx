a928d424583004d29222be2419263a03cbd5a964
TenE18 <tchinnu5132@gmail.com>
2024-10-11 18:50:21 +0530

    Fixed : URL error

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { RouterProvider, createHashRouter } from "react-router-dom";
+
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/React-Projects/">
-    <App />
-  </BrowserRouter>
+  <RouterProvider router={router} />
 );
