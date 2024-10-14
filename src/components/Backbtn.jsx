5f246c402c6c07b055af4cca1081ccbd03a0e0a3
TenE18 <tchinnu5132@gmail.com>
2024-10-14 13:03:51 +0530

    fixed: back btn

--- src/components/Backbtn.jsx
+++ src/components/Backbtn.jsx
@@ -1,19 +1,18 @@
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 
-function Backbtn(props) {
-  const { text, steps } = { text: "Go Back", steps: 1, ...props }; // Set defaults
-  // const { text, steps } = { text: "Go Back", steps: 1, ...props };
-  /* Set defaults with spread
-  Using the spread operator allows you to set default prop values while still accepting and overriding those values with any props passed to the component, enhancing flexibility and reusability.
-  */
-
-  const goback = useNavigate();
-  const location = useLocation(); // Get the current location
-  const historyLength = location.state?.historyLength || 0; // Retrieve the history length from location state
+function Backbtn({ text = "Go Back", steps = 1 }) {
+  const navigate = useNavigate();
 
   const back = () => {
-    historyLength < props.steps ? goback("/") : goback(-steps);
+    const historyLength = window.history.length;
+
+    // If the history length is less than or equal to the steps, redirect to the base URL
+    if (historyLength <= steps) {
+      window.location.href = "/React-Projects/"; 
+    } else {
+      navigate(-steps);
+    }
   };
 
   return (
